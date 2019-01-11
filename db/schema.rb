c9a9e38929ed0bf4d379c890a207b2863007e769
Reny Vary <ivarivoda1704@gmail.com>
2019-01-11 21:12:08 +0600

    schema.rb изменилась после bundle & rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2019_01_11_135447) do
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nickname"
     t.string "first_name"
@@ -27,7 +36,6 @@ ActiveRecord::Schema.define(version: 2019_01_11_135447) do
     t.integer "phone"
     t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
