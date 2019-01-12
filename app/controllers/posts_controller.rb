1d5320b616905d33460c4ff27710cfab1630de52
Джена Абишева <jenetka245@gmail.com>
2019-01-12 16:30:45 +0600

    Добавлен Active Storage&Post Controller&Post View&Routers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to user_path(@post.user)
+    else
+      render 'new'
+    end
+  end
+
+  def index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description ,:user_id, images: [])
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :user
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { in: 20..500 }
+  validate :correct_image_type
+
+  belongs_to :user
+  has_many_attached :images
+  
+  private 
+
+  def correct_image_type
+
+  	if images.attached? && !images.all? {|image| image.content_type.in?( %w( image/jpeg image/png )) }
+      errors.add(:images , 'Must be a JPEG or a PNG')
+  	elsif images.attached? == false 
+  		errors.add(:images , 'required')
+  	end
+
+  end
 end
