10be834bf86e64a529f77dc574cd97db51b5a5a8
Almat Botabekov <almatbotabekov@gmail.com>
2019-01-14 22:00:58 +0600

    создал в контролллерe posts_controller экшны update, destroy, вьюхи для них

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :users, only: :show
-  resources :posts, only: [:create, :new , :show , :index]
+  resources :posts
+  # delete 'posts/:id', to: 'posts#destroy' 
+  # resources :posts, only: [:create, :new , :show , :index, :update, :destroy, :posts_id]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
