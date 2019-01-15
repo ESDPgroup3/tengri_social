89a830d3b84e45ae119201b1a85eea61c8bf5ed3
Марина Трунова <tm20388@gmail.com>
2019-01-15 15:56:22 +0600

    добавила гем Faker, в сиды два юзера

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(phone: '1111111', password: '111111', password_confirmation: '111111')
+User.create(phone: '2222222', password: '222222', password_confirmation: '222222')
+User.create(phone: '3333333', password: '333333', password_confirmation: '333333')
+
+require 'faker'
+include Faker
+
+def copy_image_fixture(product, file)
+  fixtures_path = Rails.root.join('app', 'assets', 'images', "#{file}.jpg")
+  product.images.attach(io: File.open(fixtures_path), filename: "#{file}.jpg")
+end
+
+@file = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+10.times do
+  3.times do |n|
+    n = Post.create(
+      title: Faker::Lorem.sentence(1),
+      description: Faker::Lorem.sentence(8),
+      user_id: n
+      )
+    copy_image_fixture(n, @file.sample)
+  end
+end
