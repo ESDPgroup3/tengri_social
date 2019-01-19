a24089f45a1ffe9f8019906d371284d3c4943f5c
Марина Трунова <tm20388@gmail.com>
2019-01-19 19:09:33 +0600

    task#68 добавила обработку javascript на глазик - посмотреть скрыть оба пароля

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,14 @@ $(".toggle-password").click(function() {
   });
 });
 
+$(document).ready(function(){
+$(".toggle-password_confirmation").click(function() {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+  });
+});
