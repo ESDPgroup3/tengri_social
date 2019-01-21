5b786f4a518c23bf82d28fbd507b2e76bc3230f7
Марина Трунова <tm20388@gmail.com>
2019-01-21 19:39:55 +0600

    task#74 при создании нового пароля перенаправляет на страницу создания никнейма

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root  'authentication#login_or_signup'
-  get 'authentication/show_form', as: :show_form_in
-  post 'authentication/look_for'
-  resources :comments, only: [:new, :create]
-  resources :users, :only => [:index, :show] do
+  devise_for :users, controllers: { registrations: 'registrations' }
+  resources :users
+  resources :users  do
     member do
       get 'follows'
       get 'followers'
     end
   end
+  root  'authentication#login_or_signup'
+  get 'authentication/show_form', as: :show_form_in
+  get 'authentication/add_nickname', as: :nickname
+  post 'authentication/look_for'
+  resources :comments, only: [:new, :create]
+
   resources :posts
   get 'likes_toggle/:post_id', to: 'users#likes_toggle', as: :likes_toggle
   get 'follow_toggle/:user_id', to: 'users#follow_toggle', as: :follow_toggle
