6031a113692799648c0ce6e26a3f1704273c498a
Almat Botabekov <almatbotabekov@gmail.com>
2019-01-22 18:47:17 +0600

    сделал вьюху user.show профиль пользователя

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 100 }
+  
   validates :description, presence: true, length: { in: 5..500 }
   # validate :correct_image_type
 
