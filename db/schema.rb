7a89b1f5d395a4580bf62841f9c753cdf5b789c8
Reny Vary <ivarivoda1704@gmail.com>
2019-01-30 00:57:00 +0600

    task#91 провела миграции

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_01_23_125811) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,4 +86,5 @@ ActiveRecord::Schema.define(version: 2019_01_23_125811) do
     t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
