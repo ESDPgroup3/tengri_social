509e966468feecd3f0836dfabc77f2bad327ef8e
Reny Vary <ivarivoda1704@gmail.com>
2019-03-18 21:23:35 +0600

    task-143 в контроллере searches_controller.rb создала новый action для вывода юзеров по номеру телефона

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,4 +2,8 @@ class SearchesController < ApplicationController
   def index
     @searches = User.text_search(params[:query])
   end
+
+  def phone_search
+    @phones = User.phone_search(params[:query])
+  end
 end
