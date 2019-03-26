09cabaa34c3d6e94e72c1199bf9949dc40588b52
Reny Vary <ivarivoda1704@gmail.com>
2019-03-26 14:05:52 +0600

    task#168 пыталась понять почему не выводятся хештеги - ничего в моделе не изменилось

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,11 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :posts
-
-    def self.hashtag_search(query)
-        if query.present?
-          where("name ilike :q", q: "%#{query}%")
-        else
-          where(nil)
-        end
-    end
+  has_and_belongs_to_many :posts
+  
+  def self.hashtag_search(query)
+      if query.present?
+        where("name ilike :q", q: "%#{query}%")
+      else
+        where(nil)
+      end
+  end
 end
